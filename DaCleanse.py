04608ac7c795044d290bab167a877519c811493f
Alex Moy <35498611+alexandersmoy@users.noreply.github.com>
2019-03-19 07:24:35 -0400

    Update DaCleanse

--- DaCleanse.py
+++ DaCleanse.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import nltk
 import os
 import re
@@ -7,19 +9,28 @@ import pandas as pd
 # import numpy as np
 
 # Filepath and file name
-dir_path = os.path.dirname(os.path.realpath(__file__))
-filename = 'BL-Flickr-Images-Book.csv'
+dir_path = os.path.dirname(os.path.realpath(__file__)
+filename='BL-Flickr-Images-Book.csv'
+# ipy: full_path = r'C:\Users\atmoy\OneDrive\Desktop\Python\II-CEx\Data\BL-Flickr-Images-Book.csv'
+
 
 # Data cleaning
 # Extract - Access CSV and extract to staging
-book_df = pd.read_csv("%s\\Data\\%s" % (dir_path, filename))
+book_df=pd.read_csv("%s\\Data\\%s" % (dir_path, filename))
+# ipy: book_df = pd.read_csv(full_path)
 
 # review column headers
-list(book_df)
+book_df.columns
+
+# Evaluate if index is unique
+print("Identifier is unique - %s" %
+      (book_df.Identifier.is_unique)
+      )
+
 
 # Transform - Standardize and Clean-up
 # Drop columns / Change index to Identifier
-df = (book_df
+df=(book_df
       .drop(columns=['Edition Statement',
                      'Corporate Author',
                      'Corporate Contributors',
@@ -31,10 +42,6 @@ df = (book_df
       .set_index('Identifier')
       )
 
-# Evaluate if index is unique
-print("Index is unique - %s" %
-      (len(df.index) == len(df.index.unique()))
-      )
 
 # Clean-up inconsistent data
 # 'Date of Publication' tasks
@@ -43,22 +50,10 @@ print("Index is unique - %s" %
 #   - enforce Numeric Value
 
 # ***Using label index to reference column***
-pub_date = df.loc[:, 'Date of Publication']
+pub_date=df.loc[:, 'Date of Publication']
 
-
-# Create year extract function
-def year_extract(txt):
-    match = re.search(r'(\d{4})', str(txt))
-    if match:
-        return match.group(1)
-    else:
-        return('')
-
-
-# Apply year_extract function
-pub_date = pub_date.apply(year_extract)
-# Convert to numeric
-pd.to_numeric(pub_date)
+# Extract year and store numeric
+pub_date=pd.to_numeric(pub_date.str.extract(r'(\d{4})', expand=False))
 # pub_date finalized ^
 
 
@@ -66,15 +61,35 @@ pd.to_numeric(pub_date)
 #   - Standardize each Place
 #   - Remove extra Characters
 # ***Using position index to reference column***
-pub_place = df.iloc[:, 0]
+pub_place=df.iloc[:, 0]
 # Extract any years from pub_place for comparing to pub_date
-pub_place_date = pub_place.apply(year_extract)
+pub_place_date=pub_place.str.extract(r'(\d{4})', expand=False)
+
 # if pub_date is '' and pub_place_date is populated, use pub_place_date
-pub_date.apply()
+pub_date2=pd.Series(pub_date)
+pub_date2[pub_date.isnull()]=pub_place_date
+
+# pub_date.isnull().sum()
+# pub_date2.isnull().sum()
 
 # ************ pub_place processing ************
 # Remove extra characters and numeric values
-pub_place_dtm = pub_place
+pub_place_dtm=pd.DataFrame(pub_place)
+
+# set stopwords
+stopword_list=stopwords.words('english')
+pub_place_dtm['cleaned_place']=None
+
+# Create DTM
+for i in range(0, len(pub_place_dtm)-1):
+    # remove non-letters
+    exclusions='[^a-zA-Z]'
+    text=re.sub(exclusions, ' ', pub_place_dtm[i])
+    text=text.lower()
+    words=text.split()
+    words=[word for word in words if not word in stopword_list]
+    pub_place_dtm.iloc[i, 1]=' '.join(words)
+
 
 # Apply tokenization pub_place (1-gram, 2-gram)
 
