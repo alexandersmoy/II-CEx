c1e6382e5f3ee4cfcda59db7ec52a2a8cddd8fdf
Alex Moy <35498611+alexandersmoy@users.noreply.github.com>
2019-03-19 21:50:43 -0400

    Update - Final

--- DaCleanse.py
+++ DaCleanse.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
+# encoding: utf-8
 
 from nltk.corpus import stopwords
+# nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
+# nltk.download('punkt')
+from nltk.probability import FreqDist
 import nltk
 import os
 import re
 import pandas as pd
-# import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
 
+# ************ Read in Data ************
 # Filepath and file name
-dir_path = os.path.dirname(os.path.realpath(__file__)
-filename='BL-Flickr-Images-Book.csv'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+file_name = r'BL-Flickr-Images-Book.csv'
 # ipy: full_path = r'C:\Users\atmoy\OneDrive\Desktop\Python\II-CEx\Data\BL-Flickr-Images-Book.csv'
 
-
-# Data cleaning
-# Extract - Access CSV and extract to staging
-book_df=pd.read_csv("%s\\Data\\%s" % (dir_path, filename))
+# Extract - read CSV and extract to staging book_df
+book_df = pd.read_csv("%s\\Data\\%s" % (dir_path, file_name))
 # ipy: book_df = pd.read_csv(full_path)
 
+
+# ************ Review Data ************
 # review column headers
 book_df.columns
 
@@ -28,9 +34,9 @@ print("Identifier is unique - %s" %
       )
 
 
-# Transform - Standardize and Clean-up
+# ************ Transform/Staging ************
 # Drop columns / Change index to Identifier
-df=(book_df
+df = (book_df
       .drop(columns=['Edition Statement',
                      'Corporate Author',
                      'Corporate Contributors',
@@ -42,69 +48,128 @@ df=(book_df
       .set_index('Identifier')
       )
 
-
-# Clean-up inconsistent data
-# 'Date of Publication' tasks
+# --------------------------------------------------- #
+# -- Clean-up inconsistent data --
+# --------------------------------------------------- #
+# 'Date of Publication' tasks:
 #   - Remove Extra Characters
 #   - Use first date present
 #   - enforce Numeric Value
-
+# --------------------------------------------------- #
 # ***Using label index to reference column***
-pub_date=df.loc[:, 'Date of Publication']
+pub_date = df.loc[:, 'Date of Publication']
 
 # Extract year and store numeric
-pub_date=pd.to_numeric(pub_date.str.extract(r'(\d{4})', expand=False))
-# pub_date finalized ^
+pub_date = pd.to_numeric(pub_date.str.extract(r'(\d{4})', expand=False))
 
-
-# 'Place of Publication' tasks
+# --------------------------------------------------- #
+# 'Place of Publication' tasks:
 #   - Standardize each Place
 #   - Remove extra Characters
+# --------------------------------------------------- #
 # ***Using position index to reference column***
-pub_place=df.iloc[:, 0]
+pub_place = pd.DataFrame(df.iloc[:, 0])
 # Extract any years from pub_place for comparing to pub_date
-pub_place_date=pub_place.str.extract(r'(\d{4})', expand=False)
+pub_place_date = pub_place.iloc[:, 0].str.extract(r'(\d{4})', expand=False)
 
 # if pub_date is '' and pub_place_date is populated, use pub_place_date
-pub_date2=pd.Series(pub_date)
-pub_date2[pub_date.isnull()]=pub_place_date
+pub_date2 = pd.Series(pub_date)
+pub_date2[pub_date.isnull()] = pub_place_date
 
 # pub_date.isnull().sum()
 # pub_date2.isnull().sum()
 
-# ************ pub_place processing ************
-# Remove extra characters and numeric values
-pub_place_dtm=pd.DataFrame(pub_place)
-
+# --- pub_place processing ---
 # set stopwords
-stopword_list=stopwords.words('english')
-pub_place_dtm['cleaned_place']=None
+stopword_list = stopwords.words('english')
 
-# Create DTM
-for i in range(0, len(pub_place_dtm)-1):
+
+# Function: clean_data(row)
+#   - remove non-letters and stopwords
+def clean_data(record):
     # remove non-letters
-    exclusions='[^a-zA-Z]'
-    text=re.sub(exclusions, ' ', pub_place_dtm[i])
-    text=text.lower()
-    words=text.split()
-    words=[word for word in words if not word in stopword_list]
-    pub_place_dtm.iloc[i, 1]=' '.join(words)
+    text = re.sub('[^a-zA-Z]', ' ', record)
+    text = text.lower()
+    words = text.split()
+    words = [word for word in words if not word in stopword_list]
+    return ' '.join(words)
 
 
-# Apply tokenization pub_place (1-gram, 2-gram)
+# Clean pub_place data + store back in dataframe
+pub_place['cleaned_place'] = pub_place['Place of Publication'].apply(clean_data)
 
-# Create doc term matrix
+# Apply tokenization to pub_place (1-gram)
+pub_place['tokens'] = pub_place.cleaned_place.apply(word_tokenize)
 
-# Evaluate for terms repeating over mean frequency
+# --- Document Length Analysis ---
+# Analysis of text by number of words/tokens
+len(pub_place.tokens)
+(pub_place.tokens.apply(len) == 1).sum()
+(pub_place.tokens.apply(len) == 2).sum()
+(pub_place.tokens.apply(len) > 2).sum()
 
-# Compare 2-grams to 1-grams for each doc
+# --- Term Frequency Analysis ---
+# Evaluate term frequency
+pub_place_cat = pub_place.cleaned_place.str.cat(sep=' ')
+fdist = FreqDist(word_tokenize(pub_place_cat))
+print(fdist)
+fdist.most_common(40)
+# fdist.plot(50, cumulative=False)
 
-# Pick 2-grams over 1-gram component within each doc
+# --- Create High Freq Word List ---
+high_freq_words = [item[0] for item in fdist.most_common(40)
+                   if str(item[0]) != 'nan']
+# Hand prune high_freq_words
+high_freq_words.remove('new')
+high_freq_words.remove('york')
+high_freq_words.remove('printed')
+high_freq_words.remove('pp')
+high_freq_words.remove('mass')
+high_freq_words.remove('co')
+high_freq_words.remove('w')
+high_freq_words.remove('j')
+high_freq_words.remove('n')
+high_freq_words.remove('e')
+high_freq_words.remove('enk')
+high_freq_words.remove('kj')
+high_freq_words.remove('buenos')
+# Add to high_freq_words
+high_freq_words.append('plymouth')
+# print(high_freq_words)
 
-# Create finalized pub_place based on remaining tokens
+# --- Build pub_place['final'] ---
+# Evaluate if pub_place.tokens contains high_freq_words then use high_freq_words
+#   else use pub_place.cleaned_place
+pub_place_final = []
+for index, row in pub_place.iterrows():
+    words = [word for word in row['tokens'] if word in high_freq_words]
+    place = ''
+    if (len(words) == 0):
+        place = row['cleaned_place']
+    else:
+        place = words[0]
+    pub_place_final.append(place)
 
-# Semi-robust technique versus database lookup
+pub_place['final'] = pub_place_final
 
+# ************ Final Staging to Output ************
 # Standardize Column names
+df_final = (pd.DataFrame(df)
+            .rename(columns={
+                'Place of Publication': 'publication_place',
+                'Date of Publication': 'publication_date',
+                'Publisher': 'publisher',
+                'Title': 'title',
+                'Author': 'author',
+                'Flickr URL': 'flickr_url'
+            })
+            )
+# Assign cleaned publication_date
+df_final['publication_date'] = pub_date2
+# Assign cleaned publication_place
+df_final['publication_place'] = pub_place['final']
 
-# Load - Output file with Standardized column names (.csv / .json)
+
+# Load - Output file with Standardized column names (.csv)
+df_final.to_csv("%s\\cleaned_books.csv" % (dir_path))
+# ipy: df_final.to_csv(r'C:\Users\atmoy\OneDrive\Desktop\Python\II-CEx\cleaned_books.csv')
